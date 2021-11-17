46475048442ec93a808adec01619a9ed863c6c54
Karen Kitchens <karen.kitchens@gmail.com>
2021-11-17 10:56:35 -0600

    Add sample cuba data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import highchartsMap from "highcharts/modules/map"
 import proj4 from "proj4";
 import mapDataCU from "@highcharts/map-collection/countries/cu/cu-all.geo.json";
 
-highchartsMap(Highcharts);
-
 if (typeof window !== "undefined") {
   window.proj4 = window.proj4 || proj4;
 }
+
+let data = [
+  ['cu-ho', 0],
+  ['cu-ar', 1],
+  ['cu-ma', 2],
+  ['cu-vc', 3],
+  ['cu-5812', 4],
+  ['cu-ij', 5],
+  ['cu-ss', 6],
+  ['cu-ca', 7],
+  ['cu-cm', 8],
+  ['cu-ch', 9],
+  ['cu-cf', 10],
+  ['cu-gu', 11],
+  ['cu-gr', 12],
+  ['cu-lt', 13],
+  ['cu-sc', 14],
+  ['cu-mq', 15],
+  ['cu-pr', 16]
+];
+
 const mapOptions = {
   chart: {
     map: 'countries/cu/cu-all'
@@ -21,36 +40,37 @@ const mapOptions = {
       enabled: false
   },
   mapNavigation: {
-    enabled: true
+    enabled: true,
+    buttonOptions: {
+        verticalAlign: 'bottom'
+    }
   },
-tooltip: {
-    headerFormat: '',
-    pointFormat: '<b>{point.freq}</b><br><b>{point.keyword}</b>                      <br>lat: {point.lat}, lon: {point.lon}'
+
+  colorAxis: {
+      min: 0
   },
+  // tooltip: {
+  //   headerFormat: '',
+  //   pointFormat: '<b>{point.freq}</b><br><b>{point.keyword}</b>                      <br>lat: {point.lat}, lon: {point.lon}'
+  // },
   series: [{
-    // Use the gb-all map with no data as a basemap
-    name: 'Basemap',
     mapData: mapDataCU,
-    borderColor: '#A0A0A0',
-    nullColor: 'rgba(200, 200, 200, 0.3)',
-    showInLegend: false
-  }, {
-    // Specify points using lat/lon
-    type: 'mapbubble',
-    name: 'Cities',
-    color: '#4169E1',
-    data: [{ z: 10, keyword: "Galway", lat: 53.27, lon: -9.25 }, 
-           { z: 4, keyword: "Dublin", lat: 53.27, lon: -6.25 }],
-    cursor: 'pointer',
-    point: {
-      events: {
-        click: function() {
-          console.log(this.keyword);
+    data: data,
+    name: 'Random data',
+    states: {
+        hover: {
+            color: '#BADA55'
         }
-      }
+    },
+    dataLabels: {
+        enabled: true,
+        format: '{point.name}'
     }
   }]
 }
+
+highchartsMap(Highcharts);
+
 function App() {
   return (
     <div className="App">
