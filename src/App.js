0d08b002dae81978dcbca91feeca1d719c747b92
Karen Kitchens <karen.kitchens@gmail.com>
2021-11-16 22:52:51 -0600

    Initial react highchart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'Example chart'
+  },
+    
+  series: [{
+    data: [[Date.UTC(2013,5,2),0.7695],
+          [Date.UTC(2013,5,3),0.7648],
+          ...
+          [Date.UTC(2013,5,24),0.7623],]
+  }]
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact
+        highcharts={Highcharts}
+        constructorType={'stockChart'}
+        options={options}
+      />
+        
     </div>
   );
 }
